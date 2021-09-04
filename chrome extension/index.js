42feea7c346ed138376fafd9bab3faa2cf8f0fb5
kleenkanteen <antpalmolive@gmail.com>
2021-09-04 11:47:53 -0400

    Finish extension

--- chrome extension/index.js
+++ chrome extension/index.js
@@ -3,21 +3,30 @@ const inputEl = document.getElementById("input-el");
 let inputBtn = document.getElementById("input-btn");
 let deleteBtn = document.getElementById("delete-btn");
 let ulEl = document.getElementById("ul-el");
+let tabBtn = document.getElementById("tab-btn");
 ListItems = "";
 
 const localLeads = JSON.parse(localStorage.getItem("leads"));
 if (localLeads) {
     myLeads = localLeads;
-    render();
+    render(myLeads);
 }
 
-function render() {
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("leads", JSON.stringify(myLeads));
+        render(myLeads);
+    });
+});
+
+function render(leadsList) {
     ListItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leadsList.length; i++) {
         ListItems += `
         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
+            <a target='_blank' href='${leadsList[i]}'>
+                ${leadsList[i]}
             </a>
         </li>
         `;
@@ -29,11 +38,11 @@ inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
     localStorage.setItem("leads", JSON.stringify(myLeads));
     inputEl.value = "";
-    render();
+    render(myLeads);
 });
 
 deleteBtn.addEventListener("dblclick", function () {
     myLeads = [];
     localStorage.setItem("leads", JSON.stringify(myLeads));
-    render();
+    render(myLeads);
 });
